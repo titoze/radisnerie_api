38f03bdcbf09a67e9c5cce2659a9fec68a5f72f0
Théo Wiart <twiart@click2buy.com>
2020-08-18 00:48:59 +0200

    add credentials to .env

--- database/credentials.js
+++ database/credentials.js
@@ -2,12 +2,11 @@ require('dotenv').config()
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-  host: 'localhost',
-  database: 'radisnerie_dev',
-  port: 5432,
+  host: process.env.db_host,
+  database: process.env.db_name,
+  port: process.env.db_port,
   user: process.env.db_username,
   password: process.env.db_password
-
 })
 
 module.exports = { pool }
